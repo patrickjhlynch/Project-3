2699208e253745324c1caeb43d123e8ec444ff8f
Patrick Lynch <patrickjhlynch@gmail.com>
2017-10-30 02:44:37 +1300

    added some comments and removed some dead code

--- js/quizScript.js
+++ js/quizScript.js
@@ -2,7 +2,6 @@ var currentQuestion = 0;
 var score = 100;
 var totalQuestions = 20;
 
-var container = document.getElementById('quizContainer');
 var questionNum = document.getElementById('questionNum');
 var questionEl = document.getElementById('question');
 var image = document.getElementById('memeImage');
@@ -10,6 +9,7 @@ var opt1 = document.getElementById('opt1');
 var opt2 = document.getElementById('opt2');
 var opt3 = document.getElementById('opt3');
 var opt4 = document.getElementById('opt4');
+var opt5 = document.getElementById('opt5');
 var nextButton = document.getElementById('nextButton');
 var resultCont = document.getElementById('result');
 
@@ -18,10 +18,12 @@ function loadQuestion(questionIndex){
 	questionNum.textContent = "QUESTION " + (questionIndex + 1) + ".";
 	questionEl.textContent = q.question;
 	image.src = "img/memes/" + q.image;
+	//The responses can be altered beyond the 5 used based on what's in questions.js
 	opt1.textContent = q.option1;
 	opt2.textContent = q.option2;
 	opt3.textContent = q.option3;
 	opt4.textContent = q.option4;
+	opt5.textContent = q.option5;
 };
 
 function loadNextQuestion(){
@@ -31,11 +33,11 @@ function loadNextQuestion(){
 		return;
 	}
 	var answer = selectedOption.value;
-	if(answer == 5){
+	if(answer == 5){ //they didn't get it
 		score += 5;
-	} else if(questions[currentQuestion].answer == answer){
+	} else if(questions[currentQuestion].answer == answer){ //correct
 		score -= 5;
-	} else if(Math.abs(questions[currentQuestion].answer - answer) >= 2){
+	} else if(Math.abs(questions[currentQuestion].answer - answer) >= 2){ //off by more than 1
 		score += 5;
 	}
 	selectedOption.checked = false;
@@ -44,8 +46,8 @@ function loadNextQuestion(){
 		nextButton.childNodes[0].textContent = 'FINISH';
 	}
 	if (currentQuestion == totalQuestions){
-		localStorage.setItem("score", score);
-		window.location.href = "results.html";
+		localStorage.setItem("score", score); //Stores their score
+		window.location.href = "results.html"; //Sends  them to the results page
 	}
 	loadQuestion(currentQuestion);
 }
--- js/results.js
+++ js/results.js
@@ -1,5 +1,6 @@
 var canvas;
 var context;
+//Heights for elements of the results to make arranging easier
 var logoHeight = 30;
 var nqHeight = 350;
 var barHeight = 375;
@@ -14,17 +15,29 @@ function drawResults(){
 	var evText = document.getElementById('evaluation');
 	var score = localStorage.getItem("score");
 
+	//If a user attempts to access the results page without having a result
+	//they're redirected to the test page
+	if (score == null){
+		window.location.href = "test.html";
+	}
+
+	//Customised messages based on their score
 	if (score <= 25){
-		evText.textContent = "Yeah nice one head to the submit page if you feel like helping out";
-	}else if (score <= 75){
+		evText.textContent = "You looked at the source code or something right?";
+	} else if (score <= 50){
+		evText.textContent = "You've delved into the darkest corners of the internet and emerged a more complete human.";
+	} else if (score <= 75){
 		evText.textContent = "You know your way around a meme or two, but don't get too cocky...";
 	} else if (score <= 125){
 		evText.textContent = "Looks like you're just a regular old knuckle-dragging normie. Better luck next time!";
+	} else if (score <= 150){
+		evText.textContent = "You're not very good at this, are you?";
 	} else if (score <= 175){
 		evText.textContent = "I feel sorry for you... and your parents.";
 	} else{
 		evText.textContent = "You did this on purpose right?";
 	}
+
 	canvas=document.getElementById("canvas");
 	context=canvas.getContext("2d");
 
@@ -40,6 +53,7 @@ function drawResults(){
 		score = score*3-2; //For drawing the bar
 	}
 
+	//Draws the pink bar based on their NQ
 	var pinkBar=context.createImageData(score,43);
 	for (var i=0;i<pinkBar.data.length;i+=4){
 		pinkBar.data[i+0]=255;
@@ -55,10 +69,12 @@ function drawResults(){
 	context.font = "bold 1.25em Montserrat";
 	context.fillText("WOKE", 25, scaleHeight);
 	context.fillText("NORMIE", 318, scaleHeight);
+	//For some reason \n didn't work so just drawing text twice
 	context.fillText("TURBO", 620, scaleHeight);
 	context.fillText("NORMIE", 615, scaleHeight + 22)
 };
 
+//Fills the background in white so the image displays correctly
 function drawBG(){
 	var background=context.createImageData(720,510);
 	for (var i=0;i<background.data.length;i+=4){
@@ -70,6 +86,7 @@ function drawBG(){
 	context.putImageData(background, 0, 0);
 };
 
+//Gives them info on how to copy/save the image
 function getImage(){
 	var getImgButton = document.getElementById("shareButton");
 	var shareInfo = document.getElementById("shareInfo");
